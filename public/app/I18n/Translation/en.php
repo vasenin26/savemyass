570d0eda5c2222b70e6af9960e8c4ed77a995319
vasenin26 <leonid.vasenin@gmail.com>
2023-10-04 20:33:46 +0300

    translate to english

--- public/app/I18n/Translation/en.php
+++ public/app/I18n/Translation/en.php
@@ -10,8 +10,18 @@ return [
     'publish_options.field.delay' => 'Publish delay (days):',
     'publish_options.field.emails' => 'Emails:',
 
-    'form.action.save' => 'Save',
-
     'emails.error.field_is_empty' => 'You must set at least one email address',
     'password.error.field_is_empty' => 'You must set a password',
+
+    'congratulations.title' => 'Congratulations!',
+    'congratulations.dsc' => 'Check the settings and click "Finish" or "Restart".',
+
+    'protected.title' => 'Data is not accessible',
+    'protected.dsc' => 'Data will be published: ',
+
+    'email.subject' => 'Automatic message from:',
+
+    'form.action.save' => 'Save',
+    'form.action.finish' => 'Finish',
+    'form.action.reset' => 'Restart'
 ];
